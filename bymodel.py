e0710e01b08c322db9e255aa629ffa08e6f465b9
Juan <j.florez-velasquez@mycit.ie>
2019-10-01 15:21:07 +0200

    running, creating the test file

---
+++ bymodel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 15:11:14 2019
+
+@author: juanflorez
+Bayesian model library
+"""
+
+def read_word_dict(fileName, word_dict):
+    """
+    given a fileName, add the words to word_dict
+    TODO: improve resialance, and handle exceptions
+    """
+    return len(word_dict)
+
--- model_test.py
+++ model_test.py
@@ -5,6 +5,7 @@ Created on Tue Oct  1 13:41:41 2019
 
 @author: juanflorez
 """
+import bymodel as bym
 
 # Create a adictionary of words key = word and amount
 
@@ -13,10 +14,12 @@ targetDirectory = {"the":2, "a":4, "is":1, "word":2, "no":1}
 
 def test_build_read_word_dict():
     fileName = "input_test.txt"
-    with open(fileName) as outp:
+    with open(fileName,"a") as outp:
         outp.write(testPhrase)
     word_dict = {}
-    bymodel.read_word_dict(fileName, word_dict)
+    bym.read_word_dict(fileName, word_dict)
     assert(targetDirectory == word_dict)
 
+
+test_build_read_word_dict()
 
