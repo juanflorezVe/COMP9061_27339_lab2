15889b2e520c530cfe63bd1f4b93eb19c17e467d
Juan <j.florez-velasquez@mycit.ie>
2019-10-01 23:40:47 +0200

    code beautification pep8

--- model_test.py
+++ model_test.py
@@ -10,11 +10,15 @@ import bymodel as bym
 # Create a adictionary of words key = word and amount
 
 testPhrase = "the the a a a is a word words no"
-targetDirectory = {"the":2, "a":4, "is":1, "word":1, "no":1, "words":1}
+targetDirectory = {"the": 2, "a": 4, "is": 1, "word": 1, "no": 1, "words": 1}
+
 
 def test_build_read_word_dict():
+    """
+    Create a file, build a dictionay
+    """
     fileName = "input_test.txt"
-    with open(fileName,"w") as outp:
+    with open(fileName, "w") as outp:
         outp.write(testPhrase)
     word_dict = {}
     count = bym.read_word_dict(fileName, word_dict)
@@ -23,4 +27,3 @@ def test_build_read_word_dict():
 
 
 test_build_read_word_dict()
-
