6eb51c074fe89c86cbbc94d90c48a7a05ad7d2f6
juan <florez.esteban@gmail.com>
2019-10-01 14:50:15 +0200

    first test

---
+++ model_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 13:41:41 2019
+
+@author: juanflorez
+"""
+
+# Create a adictionary of words key = word and amount
+
+testPhrase = "the the a a a is a word words no"
+targetDirectory = {"the":2, "a":4, "is":1, "word":2, "no":1}
+
+def test_build_read_word_dict():
+    fileName = "input_test.txt"
+    with open(fileName) as outp:
+        outp.write(testPhrase)
+    word_dict = {}
+    bymodel.read_word_dict(fileName, word_dict)
+    assert(targetDirectory == word_dict)
+
+
