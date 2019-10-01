e5040d87020b850e06e59e5f03b435ee28cf6ba5
Juan <j.florez-velasquez@mycit.ie>
2019-10-01 15:45:26 +0200

    corrected error in test dictionay, implemented v1 of read_word_dic

--- bymodel.py
+++ bymodel.py
@@ -12,5 +12,18 @@ def read_word_dict(fileName, word_dict):
     given a fileName, add the words to word_dict
     TODO: improve resialance, and handle exceptions
     """
+    with open(fileName, "r") as fl:
+        line_buffer = fl.readlines()
+        for l in line_buffer:
+            tmp_words = l.split(' ')
+            
+            for w in tmp_words:
+                if w not in word_dict.keys():
+                    word_dict[w] = 1
+                else:
+                    word_dict[w] += 1
+            
+            
+            print(tmp_words)
     return len(word_dict)
 
--- model_test.py
+++ model_test.py
@@ -10,15 +10,16 @@ import bymodel as bym
 # Create a adictionary of words key = word and amount
 
 testPhrase = "the the a a a is a word words no"
-targetDirectory = {"the":2, "a":4, "is":1, "word":2, "no":1}
+targetDirectory = {"the":2, "a":4, "is":1, "word":1, "no":1, "words":1}
 
 def test_build_read_word_dict():
     fileName = "input_test.txt"
-    with open(fileName,"a") as outp:
+    with open(fileName,"w") as outp:
         outp.write(testPhrase)
     word_dict = {}
-    bym.read_word_dict(fileName, word_dict)
+    count = bym.read_word_dict(fileName, word_dict)
     assert(targetDirectory == word_dict)
+    assert(count == 6)
 
 
 test_build_read_word_dict()
