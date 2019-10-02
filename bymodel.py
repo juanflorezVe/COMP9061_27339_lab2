670d896f56d4fd0be91362a81abc02be3e2e7c5d
Juan <j.florez-velasquez@mycit.ie>
2019-10-02 11:13:03 +0200

    get a set of words from a directory

--- bymodel.py
+++ bymodel.py
@@ -6,9 +6,10 @@ Created on Tue Oct  1 15:11:14 2019
 @author: juanflorez
 Bayesian model library
 """
+from os import walk
 
 
-def read_word_dict(fileName, word_dict):
+def count_word_dict(fileName, word_dict):
     """
     Given a fileName, add the words to word_dict
     TODO: improve resialance, and handle exceptions
@@ -26,3 +27,25 @@ def read_word_dict(fileName, word_dict):
 
             print(tmp_words)
     return len(word_dict)
+
+
+def build_full_dic(path, word_bag):
+    """
+    Given a path to a directory, get the set of all words in the directory
+    and store them in word_bag dictionary
+    """
+    # build a list of files
+    files = []
+    for (dirpath, dirnames, filenames) in walk(path):
+        files.extend(filenames)
+        break
+    word_set = set()
+    print("start the loop")
+    for f in files:
+        tmp_words = set(open(path+"/"+f).read().split())
+        word_set = word_set.union(word_set, tmp_words)
+        print(word_set)
+
+    word_bag.update(dict.fromkeys(word_set, 0))
+
+    return len(word_bag)
--- model_test.py
+++ model_test.py
@@ -6,24 +6,57 @@ Created on Tue Oct  1 13:41:41 2019
 @author: juanflorez
 """
 import bymodel as bym
+import os
 
 # Create a adictionary of words key = word and amount
 
 testPhrase = "the the a a a is a word words no"
-targetDirectory = {"the": 2, "a": 4, "is": 1, "word": 1, "no": 1, "words": 1}
+testPhrase2 = "some words are common others are not"
+
+targetDictionary = {"the": 2, "a": 4, "is": 1, "word": 1, "no": 1, "words": 1}
+targetWordBag = {"the": 0, "a": 0, "is": 0, "word": 0, "words": 0, "no": 0,
+                 "some": 0, "are": 0, "common": 0, "others": 0, "not": 0}
+
+path = "tmptestdir"
+fileName1 = path+"/input_test1.txt"
+fileName2 = path+"/input_test2.txt"
+
+
+def create_test_data_directory():
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Creation of the directory %s failed" % path)
+    else:
+        print("Successfully created the directory %s " % path)
+
+    with open(fileName1, "w") as fl1:
+        fl1.write(testPhrase)
+
+    with open(fileName2, "w") as fl2:
+        fl2.write(testPhrase2)
+
+
+def test_build_full_dictionary():
+    """
+    Given a folder, get all the words in the folder and
+    make a dictionary initialized to 0
+    """
+    create_test_data_directory()
+
+    word_bag = {}
+    count = bym.build_full_dic(path, word_bag)
+    assert(word_bag == targetWordBag)
+    assert(11 == count)
 
 
 def test_build_read_word_dict():
     """
     Create a file, build a dictionay
     """
-    fileName = "input_test.txt"
-    with open(fileName, "w") as outp:
-        outp.write(testPhrase)
+    create_test_data_directory()
     word_dict = {}
-    count = bym.read_word_dict(fileName, word_dict)
-    assert(targetDirectory == word_dict)
+    count = bym.count_word_dict(fileName1, word_dict)
+    assert(targetDictionary == word_dict)
     assert(count == 6)
-
-
-test_build_read_word_dict()
