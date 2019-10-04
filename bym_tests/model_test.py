415cd145009ae9e546e90839886073b7d1d5b655
Juan <j.florez-velasquez@mycit.ie>
2019-10-04 16:08:04 +0200

    Modified tests to mimmic the material of the Bayesian classification part 2 lecture. ONGOING!

--- bym_tests/model_test.py
+++ bym_tests/model_test.py
@@ -10,160 +10,184 @@ import os
 import pytest
 
 # Create a adictionary of words key = word and amount
+"""
+Cloud Java Cloud
+Comp
+2
+Cloud Cloud Spring
+Comp
+3
+Cloud Software
+Comp
+4
+Referendum Software Election
+Politics
+Test
+5
+Java Software Java Election
+
+
+"""
 
 
 @pytest.fixture
-def docClass1():
-    return "the the a a a is a word words no"
+def doc1():  #Comp
+    return "Cloud Java Cloud"
 
 @pytest.fixture
-def docClass2():
-    return "some words are common others are not"
-
-w_count_c1 = {"the": 2, "a": 4, "is": 1, "word": 1, "no": 1, "words": 1}
+def doc2():  #Comp
+    return "Cloud Cloud Spring"
 
 @pytest.fixture
-def w_count_c2():
-    return {"some": 1, "words": 1, "are": 2, "common": 1, "others":1, "not":1,
-            "the": 0, "a": 0, "is": 0, "word": 0, "no": 0 }
+def doc3():  #Comp
+    return "Cloud Software"
 
 @pytest.fixture
-def fullNegDictionary():
-    return {"some": 0, "words": 0, "are": 0, "common": 0,
-              "others":0, "not":0 }
-    
-@pytest.fixture
-def fullDictionary():
-    return {"the": 0, "a": 0, "is": 0, "word": 0, "words": 0, "no": 0,
-                 "some": 0, "are": 0, "common": 0, "others": 0, "not": 0}
+def doc4():  #Politics
+    return "Referendum Software Election"
 
 @pytest.fixture
-def total_w():
-    return 11
+def doc5():
+    return "Java Software Java Election"
+
+@pytest.fixture
+def dict_univ():
+    """
+    Universal dictionary, all the words in all the documents of all
+    classes
+    """
+    return {"Cloud": 0, "Spring": 0, "Software": 0, "Referendum": 0,
+            "Java": 0, "Election": 0}
+
+@pytest.fixture
+def u_dict_w_count_comp():
+    """
+    Dictionay with all the universal words but having amount in comp class
+    """
+    return {"Cloud": 5, "Spring": 1, "Software": 1, "Referendum": 0,
+            "Java": 1, "Election": 0}
+
+@pytest.fixture
+def u_dict_w_count_politics():
+    """
+    Dictionay with all the universal words but having amount in politics class
+    """
+    return {"Cloud": 0, "Spring": 0, "Software": 1, "Referendum": 1,
+            "Java": 0, "Election": 1}
+
+@pytest.fixture
+def dict_w_count_comp():
+    """
+    Dictionay with the words in comp class
+    """
+    return {"Cloud": 5, "Spring": 1, "Software": 1,
+            "Java": 1}
+
+@pytest.fixture
+def dict_w_count_politics():
+    """
+    Dictionay with all the words in politics class
+    """
+    return {"Software": 1, "Referendum": 1, "Election": 1}
+
+@pytest.fixture
+def total_words_politics(u_dict_w_count_politics):
+    return sum(u_dict_w_count_politics.val())
+
+@pytest.fixture
+def total_words_comp(u_dict_w_count_comp):
+    return sum(u_dict_w_count_comp.val())
 
 # """ These are constant and are easy to keep just like this """"
 
-path = "temptest"
-path_pos = path+"/pos"
-path_neg = path+"/neg"
+path = "exmpleTest"
+path_comp = path+"/comp"
+path_pol = path+"/pol"
 
-filePos1 = path_pos+"/input_test1.txt"
-fileNeg1 = path_neg+"/input_test2.txt"
-filePos2 = path_pos+"/input_test3.txt"
+fileComp1 = path_comp+"/comp_test1.txt"
+fileComp2 = path_comp+"/comp_test2.txt"
+fileComp3 = path_comp+"/comp_test3.txt"
+filePol1  = path_pol+"/pol_test1.txt"
 
 # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 
-def create_test_data_directory(docClass1, docClass2):
+def create_test_data_directory(doc1, doc2, doc3, doc4):
 
     try:
         os.mkdir(path)
-        os.makedirs(path_pos)
-        os.makedirs(path_neg)
+        os.makedirs(path_comp)
+        os.makedirs(path_pol)
     except OSError:
         print("Creation of the directory {} failed ".format(OSError))
     else:
         print("Successfully created the directory %s " % path)
 
-    with open(filePos1, "w") as fl1:
-        fl1.write(docClass1)
+    with open(fileComp1, "w") as fl1:
+        fl1.write(doc1)
 
-    with open(fileNeg1, "w") as fl2:
-        fl2.write(docClass2)
-    
-    with open(filePos2, "w") as fl3:
-        fl3.write("")
+    with open(fileComp2, "w") as fl2:
+        fl2.write(doc2)
+        
+    with open(fileComp3, "w") as fl3:
+        fl3.write(doc3)
+ 
+    with open(filePol1, "w") as fl4:
+        fl4.write(doc4)
 
 
-def test_build_full_dictionary(docClass1, fullNegDictionary, docClass2):
+def test_build_full_dictionary(doc1, doc2, doc3, doc4, dict_w_count_comp):
     """
-    Given a folder, get all the words in the folder and
+    Given a folder, get all the words in all the docs in the folder and
     make a dictionary initialized to 0
     """
-    create_test_data_directory(docClass1, docClass2)
+    create_test_data_directory(doc1, doc2, doc3, doc4)
 
-    neg_word_bag = {}
-    count = bym.build_full_dic(path_neg, neg_word_bag)
-    assert(neg_word_bag == fullNegDictionary)
-    assert(6 == count)
+    comp_word_bag = {}
+    count = bym.build_full_dic(path_comp, comp_word_bag)
+    assert(comp_word_bag == dict.fromkeys(dict_w_count_comp, 0))
+    assert(4 == count)
 
 
-def test_build_read_word_dict(docClass1, docClass2):
+def test_build_read_word_dict(doc1, doc2, doc3, doc4 , dict_univ):
     """
     Count the words in a document and update the dictionary
     """
-    create_test_data_directory(docClass1, docClass2)
-    word_dict = {}
-    count = bym.count_word_dict(filePos1, word_dict)
-    assert(w_count_c1 == word_dict)
+    create_test_data_directory(doc1, doc2, doc3, doc4)
+    word_dict = dict_univ.copy()
+    fileComp1_dict = {"Cloud": 2, "Spring": 0, "Software": 0,
+            "Referendum": 0, "Java": 1, "Election": 0}
+    count = bym.count_word_dict(fileComp1, word_dict)
+    assert(fileComp1_dict == word_dict)
     assert(count == 6)
 
 
-def test_count_words_in_directory(docClass1, fullDictionary, docClass2):
+def test_count_words_in_directory(doc1, doc2, doc3, doc4, dict_univ, 
+                                  u_dict_w_count_comp):
     """
     given a directory, and a dictionary of words,
     record the occurrence of each word in all the files in the directory.
     """
-    create_test_data_directory(docClass1, docClass2)
-    counted_words = fullDictionary.copy()  #Always create a copy 
+    create_test_data_directory(doc1, doc2, doc3, doc4)
+    counted_words = dict_univ.copy()  #Always create a copy 
 
-    total_words = bym.count_words(path_neg, counted_words)
-    targetWordCount = {"the": 0, "a": 0, "is": 0, "word": 0, "words": 1,
-                       "no": 0, "some": 1, "are": 2, "common": 1, "others": 1,
-                       "not": 1}
-    assert(counted_words == targetWordCount)
-    assert(total_words == 11)
+    total_words = bym.count_words(path_comp, counted_words)
+ 
+    assert(counted_words == u_dict_w_count_comp)
+    assert(total_words == len(counted_words))
+    
 
-
-def test_prob_word_given_class(docClass1, w_count_c2, total_w):
+def test_prob_word_given_class(u_dict_w_count_comp, dict_univ):
     """
     Given a word, and a class, calculate the number p; 0 <= p <= 1
     count of appearences of w in all the docs in c, over the total
     amount of words in c.
-    
+    P(w|c) = count_of(w) / total of words in c
     """
 
-    class2 = bym.classification("class2")
-    assert(class2.name == "class2")
-    
-    p = bym.single_w_c("common", w_count_c2, total_w)
-    manual_calc = ( w_count_c2["common"] + 1) / (sum(w_count_c2.values()) + total_w)
+    denom_normal = sum(dict_univ.values())
+    p = bym.single_w_c("Java", u_dict_w_count_comp, denom_normal)
+    manual_calc = ( u_dict_w_count_comp["Java"] + 1) / (sum(u_dict_w_count_comp.values()) + denom_normal)
                  
                  
     assert(manual_calc == p)
-
-def test_prob_c():
-    """
-    out off all the documents D, what are the odds that a document d belongs
-    to a class c
-    P(c) = total(c)/total(D)
-    assume the model has directories per class, so the probability is the total
-    of files in the class\' directory over total of documents in all
-    directories
-    
-    """
-    
-    p = bym.prob_class(path, path_pos)
-    assert(2/3 == p)
-
-
-def test_make_dic_w_c(fullDictionary, w_count_c2):
-    """
-    P(w|c) for each word in the dictionary, for the given class
-    """
-    neg_prob = fullDictionary.copy()
-    target_prob = {"the": 0, "a": 0, "is": 0, "word": 0, "no": 0,
-                   "some": 1/7, "words": 1/7, "are": 2/7, "common": 1/7,
-                   "others":1/7, "not":1/7 }
-    
-    bym.make_dic_w_c(neg_prob,  w_count_c2, 7)
-    assert(neg_prob == target_prob)
-
-def test_prob_w_c():
-    """
-    Given a word w, what are the odds that it is in a document of class c?
-    """
-    negative = bym.classification("negative doc")
-    pwn = bym.prob_w_c("common", negative)
-    
-    assert(1 == pwn)
